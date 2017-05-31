fe403b27621db8f58466f4ca36b7f6e4896d7637
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-05-30 21:57:48 -0300

    Adicionar a estrutura inicial do frontend

---
+++ front-end/src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      false
+    )
+  }
+}
