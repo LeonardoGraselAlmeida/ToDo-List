d77523fb14f5bb9488cf59a56f5a591f0d98c264
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-07 15:08:39 -0300

    Ajustar o component app para renderizar o Menu e o componente das rotas.

--- front-end/src/main/app.jsx
+++ front-end/src/main/app.jsx
@@ -1,11 +1,13 @@
-import 'modules/bootstrap/dist/css/bootstrap.min.css';
-import 'modules/font-awesome/css/font-awesome.min.css';
-import React, {Component} from 'react';
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
 
-export default class App extends Component {
-  render() {
-    return (
-      false
-    )
-  }
-}
+import React from 'react'
+import Menu from '../template/menu'
+import Routes from './routes'
+
+export default props => (
+    <div className='container'>
+        <Menu />
+        <Routes />
+    </div>
+)
