274ac4ba812efb734b10158c4f0a85bf5c601f0d
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-19 13:33:23 -0300

    Incluir os imports e configurações necessárias para trabalhar com redux.

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-import App from './main/app';
-import reducers from './main/reducers';
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
+import App from './main/app'
+import reducers from './main/reducers'
 
-ReactDom.render(<Provider store={createStore(reducers)}><App/></Provider>, document.getElementById('app'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('app'))
